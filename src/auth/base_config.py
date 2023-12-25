b643a1021c63d18017c47e196027027a385379ef
Danil Petrushkin <danil.petrushkin@mail.ru>
2023-12-25 16:32:32 +0300

    feature: add all auth jwt, bearer

--- src/auth/base_config.py
+++ src/auth/base_config.py
@@ -5,7 +5,7 @@ from src.auth.manager import get_user_manager
 from src.auth.model import User
 from src.config import SECRET_AUTH
 
-cookie_transport = CookieTransport(cookie_max_age=3600, cookie_name='bonds')
+bearer_transport = BearerTransport(tokenUrl='auth/login')
 
 
 def get_jwt_strategy() -> JWTStrategy:
@@ -15,7 +15,7 @@ def get_jwt_strategy() -> JWTStrategy:
 
 auth_backend = AuthenticationBackend(
     name='jwt',
-    transport=cookie_transport,
+    transport=bearer_transport,
     get_strategy=get_jwt_strategy
 )
 
